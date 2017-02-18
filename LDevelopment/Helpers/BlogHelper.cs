0cdaf94cb4dede682eb32e919aafdb6f5da8033f
LDVLPR <lukaszdudziak@outlook.com>
2017-02-18 17:36:52 +0100

    URL Generating - bug fix

--- LDevelopment/Helpers/BlogHelper.cs
+++ LDevelopment/Helpers/BlogHelper.cs
@@ -11,7 +11,7 @@ namespace LDevelopment.Helpers
             var specialCharacters = new Regex("(?:[^a-z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
             result = specialCharacters.Replace(result, string.Empty);
 
-            var whitespaces = new Regex("[ ]{1,}");
+            var whitespaces = new Regex("[ ]{1,}", RegexOptions.None);
             result = whitespaces.Replace(result, "-");
 
             return result.ToLower();
